f7fffe49b51db442ee5d8b3d4ab592b73fe2eed5
Fin M <fin@mace.law>
2025-06-21 19:56:51 -0400

    create chat.db if it doesnt exist

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from tinydb import TinyDB, Query
+from os import path, curdir
+
+cd = path.abspath(curdir)
+class Paths:
+    chatdb = path.join(cd, "chat.db")
+
+# check if chat.db is a thing
+if not path.isfile(Paths.chatdb):
+    open(Paths.chatdb, 'w').close() # creates that file
+    print("chat.db created")
