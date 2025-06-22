960b12f9760ce679211cd1ef9c440f34cf64d215
Fin M <fin@mace.law>
2025-06-21 20:08:38 -0400

    add defaults for the dbs

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ class Paths:
     chatdb = path.join(cd, "chat.db")
     accdb = path.join(cd, "accounts.db")
 
+def chatdb_new(chatdb: TinyDB): # defaults
+    ...
+def accdb_new(accdb: TinyDB): # defaults
+    ...
+
 # check if chat.db is a thing
 if not path.isfile(Paths.chatdb):
     open(Paths.chatdb, 'w').close() # creates that file
@@ -17,4 +22,6 @@ if not path.isfile(Paths.accdb):
     print("accounts.db created")
 
 db = TinyDB(Paths.chatdb)
+chatdb_new(db)
 accdb = TinyDB(Paths.accdb)
+accdb_new(accdb)
