c82b1d974c03b716b962a1869bc27c1710a56f57
Fin M <fin@mace.law>
2025-06-21 20:03:29 -0400

    add support for accdb

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from os import path, curdir
 cd = path.abspath(curdir)
 class Paths:
     chatdb = path.join(cd, "chat.db")
+    accdb = path.join(cd, "accounts.db")
 
 # check if chat.db is a thing
 if not path.isfile(Paths.chatdb):
     open(Paths.chatdb, 'w').close() # creates that file
     print("chat.db created")
 
+# check if accounts.db is a thing
+if not path.isfile(Paths.accdb):
+    open(Paths.accdb, 'w').close() # creates that file
+    print("accounts.db created")
+
 db = TinyDB(Paths.chatdb)
+accdb = TinyDB(Paths.accdb)
